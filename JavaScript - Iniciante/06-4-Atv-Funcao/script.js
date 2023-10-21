687647ac07ea0217b98c6e7fb67fd141ec61bd3f
João Pedro O. Braga <127335857+ojpbraga@users.noreply.github.com>
2023-10-20 22:16:49 -0300

    Eventos, Window, Funcao, Condicionais e Array
    Compartilho meus ultimos estudos e exercícios realizados através do curso Origamid.

--- JavaScript - Iniciante/06-4-Atv-Funcao/script.js
+++ JavaScript - Iniciante/06-4-Atv-Funcao/script.js
@@ -1,12 +1,19 @@
-function coordenadas(event) {
-    const circulo = document.querySelector(".circulo");
+// Selecionar qual elemento iremos usar:
+const circulo = document.querySelector(".circulo");
+
+// Função que executa quando o evento ocorrer
+function novoMouse(event) {
+
+    // Obtendo as coordenadas x e y do mouse
     y = event.y;
     x = event.x;
     console.log(x, y);
 
+    // Transferindo as coordenadas do mouse para a localização do circulo
     circulo.style.top = y + "px";
     circulo.style.left = x + "px";
 
 }
 
-window.addEventListener('mousemove', coordenadas);
+// Adicionando o evento (casoa algo ocorra, faça)
+window.addEventListener('mousemove', novoMouse);
