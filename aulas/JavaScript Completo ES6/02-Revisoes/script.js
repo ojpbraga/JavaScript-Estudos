0a5ec013a4c14d3d457f907e6c247383f600f944
ojpbraga <ojpbraga@gmail.com>
2023-12-01 21:48:05 -0300

    revision: Atribuição e Ternário

--- aulas/JavaScript Completo ES6/02-Revisoes/script.js
+++ aulas/JavaScript Completo ES6/02-Revisoes/script.js
@@ -364,3 +364,43 @@ for(var i = 0; i < frutas.length; i++) {
 
 var ultimaFruta = frutas[frutas.length - 1];
 console.log(ultimaFruta);
+
+// Atribuição e Ternário
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+// Operador Ternário
+// Abrevia as condições com if e else
+// Não faz sentido utilizar pra boolean
+var idade = 20;
+diabetes = false;
+
+var podeBeber = idade >= 18 && diabetes === false ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+
+// Outra forma de fazer o if
+if(idade >= 18)
+    console.log('Pode')
+else
+    console.log('Não pode')
+
+// Exercício
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+console.log(scroll += 500);
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+var resultadoCredito = possuiCasa && possuiCarro;
+console.log(resultadoCredito);
