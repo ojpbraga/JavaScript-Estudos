dc234a3e6c9cf3dacd3d6457d6119843e845f93a
ojpbraga <ojpbraga@gmail.com>
2024-02-29 20:41:49 -0300

    exercise: objects methods

--- aulas/JavaScript Completo ES6/0409-object/script.js
+++ aulas/JavaScript Completo ES6/0409-object/script.js
@@ -259,3 +259,50 @@ const carroLuxo = {marca: 'Ford'};
 console.log(carroLuxo.toString());
 console.log(typeof carroLuxo);
 console.log(Object.prototype.toString.call(carroLuxo));
+
+
+// Exercícios
+// Crie uma função que verifique
+// corretamente o tipo de dado
+
+function tipoDado(dado) {
+    return Object.prototype.toString.call(dado);
+}
+
+console.log(tipoDado("String"));
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+const quadrado = {};
+
+Object.defineProperties(quadrado, {
+    lados: {
+        value: 4,
+        enumerable: true,
+        configurable: false,
+    },
+});
+
+quadrado.lados = 5;
+console.log(quadrado);
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+    width: 800,
+    height: 600,
+    background: '#333'
+}
+
+Object.freeze(configuracao);
+
+  
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
+  
