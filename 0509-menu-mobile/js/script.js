929e067772ea549c84b73bb765c42e191643d12c
ojpbraga <ojpbraga@gmail.com>
2024-03-20 09:46:52 -0300

    study: menu mobile

---
+++ 0509-menu-mobile/js/script.js
@@ -0,0 +1,25 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js'
+
+import * as teste from './modules/teste.js';
+
+teste.teste1()
+teste.teste2()
+console.log(teste.senha);
+
+console.log(this)
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
